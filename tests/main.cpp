01189ea0cc7ed0ddfe49c968bea36b72ee679276
Colin Atkinson <colin.william.atkinson@gmail.com>
2019-12-01 22:28:28 -0500

    test: add basic functionality test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,9 +1,9 @@
-#include <algorithm>
-#include <cassert>
-#include <fstream>
-#include <iostream>
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
 #include <win_handle_getter.h>
+
+#include <string>
 
 // Adapted from https://stackoverflow.com/a/874160
 inline bool ends_with(
@@ -18,23 +18,29 @@ inline bool ends_with(
     }
 }
 
-int main()
+TEST_CASE("Simple file opening test", "[win_handle_getter]")
 {
     constexpr wchar_t fname[] = L"file.txt";
     const auto initial_paths = get_cur_proc_handle_paths();
 
     // Check that file.txt doesn't show up as open
-    assert(!std::any_of(initial_paths.cbegin(), initial_paths.cend(),
+    REQUIRE_FALSE(std::any_of(initial_paths.cbegin(), initial_paths.cend(),
         [&fname](const auto &path) { return ends_with(path, fname); }));
 
     // Open a file so we can see its handle in the list
     std::ofstream st(fname);
 
-    const auto final_paths = get_cur_proc_handle_paths();
+    const auto file_open_paths = get_cur_proc_handle_paths();
 
     // Check that file.txt now shows up as open
-    assert(std::any_of(final_paths.cbegin(), final_paths.cend(),
+    REQUIRE(std::any_of(file_open_paths.cbegin(), file_open_paths.cend(),
         [&fname](const auto &path) { return ends_with(path, fname); }));
 
-    return 0;
+    // Close the file and check again
+    st.close();
+    const auto file_closed_paths = get_cur_proc_handle_paths();
+
+    REQUIRE_FALSE(
+        std::any_of(file_closed_paths.cbegin(), file_closed_paths.cend(),
+            [&fname](const auto &path) { return ends_with(path, fname); }));
 }
